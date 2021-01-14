7748d171fb6c0729f12e9e005242feaa8cbf22c6
Jadielson <jadielsonnsouza@gmail.com>
2021-01-14 20:07:31 -0300

    Create Vetores.java

---
+++ src/DateArrayString/Vetores.java
@@ -0,0 +1,20 @@
+package DateArrayString;
+
+public class Vetores {
+    public static void main(String[] args){
+
+        int nota[] = {80, 70, 90, 85, 100};
+        int soma = 0;
+
+        //Percorrendo todos os valores armazenados no vetor
+        for(int i = 0; i < nota.length; i++){
+            System.out.println("Nota " + i + " = " + nota[i]);
+            soma += nota[i];
+        }
+
+        double media = soma / (double) nota.length;
+
+        System.out.println("Média Aritmética Simples = " + media);
+
+    }
+}
