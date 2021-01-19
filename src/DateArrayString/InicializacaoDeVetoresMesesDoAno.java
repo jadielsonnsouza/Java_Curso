55068a759505116ea067b0b9fbc04cfc8a44582b
Jadielson <jadielsonnsouza@gmail.com>
2021-01-19 19:31:18 -0300

    Create InicializacaoDeVetoresMesesDoAno.java

---
+++ src/DateArrayString/InicializacaoDeVetoresMesesDoAno.java
@@ -0,0 +1,19 @@
+package DateArrayString;
+
+import javax.swing.*;
+
+public class InicializacaoDeVetoresMesesDoAno {
+    public static void main(String[] args){
+
+        //declarando e inicializando os vetores 'mes' e 'diasMes'
+        String mes[] = {"janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto",
+                        "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+        int diasMes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        for(int i = 0; i < 12; i++){
+            System.out.printf("Mês %2d = %9s, tem %d dias.\n", (i+1), mes[i], diasMes[i]);
+        }
+
+    }
+}
