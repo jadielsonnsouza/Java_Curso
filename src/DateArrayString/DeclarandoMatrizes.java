4ec4dc120df74911eb61d0b975b82086c4579250
Jadielson <jadielsonnsouza@gmail.com>
2021-01-22 21:14:45 -0300

    Create DeclarandoMatrizes.java

---
+++ src/DateArrayString/DeclarandoMatrizes.java
@@ -0,0 +1,19 @@
+package DateArrayString;
+
+public class DeclarandoMatrizes {
+    public static void main(String[] args){
+
+        int matriz[][] = new int[2][4];
+        matriz[1][1] = 112;
+
+        System.out.println(matriz[1][1]);
+
+        double matrizA[][] = new double[3][5];
+        matrizA[2][2] = 154.87;
+        System.out.println(matrizA[2][2]);
+
+        int m [][]= { {1,2} , {3,4} , {5,6}};
+        System.out.println(m[1][0]);
+
+    }
+}
