bb025638a1433d92ef39a91f8f702cec6c997ed4
Jadielson <jadielsonnsouza@gmail.com>
2021-02-01 20:56:28 -0300

    Create MatrizesTamanhosDiferentes.java

---
+++ src/DateArrayString/MatrizesTamanhosDiferentes.java
@@ -0,0 +1,14 @@
+package DateArrayString;
+
+public class MatrizesTamanhosDiferentes {
+    public static void main(String[] args){
+
+        int matrizA[][] = new int[3][];     //Cria 3 linhas
+            matrizA[0] = new int[5];        //Cria 5 colunas para a linha 0
+            matrizA[1] = new int[2];        //Cria 2 colunas para a linha 1
+            matrizA[2] = new int[3];        //Cria 3 colunas para a linha 2
+
+        int matrizB[][] = {{1,2} , {1,2,3}};
+
+    }
+}
