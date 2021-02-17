4854fb2e585d7462f581ec265543316945a68ee6
Jadielson <jadielsonnsouza@gmail.com>
2021-02-16 21:05:08 -0300

    add metodo length()

--- src/DateArrayString/String/MetodosClasseString2.java
+++ src/DateArrayString/String/MetodosClasseString2.java
@@ -3,14 +3,24 @@ package DateArrayString.String;
 public class MetodosClasseString2 {
     public static void main(String[] args){
 
+        //Método trim()
         String texto = " Utilizando o método trim() ";
         System.out.println(texto);
         texto = texto.trim();
         System.out.println(texto);
 
+        //Método contains()
         texto = "001MARCOS PAULO      M19803112";
         String procurado = "MARCOS";
-        System.out.println("\n" + texto.contains(procurado));
+        System.out.println("\nUtilizando contains() = " + texto.contains(procurado) + "\n");
+
+        //Método length()
+        System.out.print("Utilizando length() = ");
+        System.out.println(" Java ".length());
+        System.out.print("Utilizando trim() & length() = ");
+        System.out.println(" Java ".trim().length());
+
+        
 
     }
 }
